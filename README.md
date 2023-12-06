b3c6ed89d01e2070ae4456e344f60d4c74952475
Edwin Anciani <edwinanciani@gmail.com>
2023-12-06 13:10:20 -0600

    Custom JWT Token Example

---
+++ README.md
@@ -0,0 +1,8 @@
+# Formio Custom JWT Token Example
+
+## Documentation
+Custom JWT token is an authentication method for any Deployed Formio Environment. Refer to documentation: [Here](https://help.form.io/developers/integrations/auth#custom-jwt-authentication)
+
+## Use
+### Run Command 
+`node index.js PROJECT_ID=[PROJECT_ID] ROLE_ID=[ROLE_ID]`
---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const secret = `${process.env.JWT_SECRET}`
+const project_id = `${process.env.PROJECT_ID}`
+const username = `${process.env.USERNAME}`
+const roles = `${process.env.ROLES}`
+var token = jwt.sign({
+  external: true,
+  project: {
+    _id: project_id,
+  },
+  user: {
+    _id: 'external',
+    data: {
+      name: username,
+    },
+    roles: roles.split(','),
+  }
+}, secret)
+console.log(token);
